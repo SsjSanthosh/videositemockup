d498a8f4b353c4c3270932f8fa1c97dd5896506b
Dextrous <ssjdexucyche@gmail.com>
2019-12-01 20:04:03 +0530

    Updates

---
+++ precache-manifest.aadbd7377b4bb4500ab1bad9d038c697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d353890fd1ae87ffaa2018ae7bfb3c22",
+    "url": "/videositemockup/index.html"
+  },
+  {
+    "revision": "1224436ef49e931584a2",
+    "url": "/videositemockup/static/css/main.c267d396.chunk.css"
+  },
+  {
+    "revision": "3472ae5e9b5ad5fa8d3c",
+    "url": "/videositemockup/static/js/2.b7dc29ef.chunk.js"
+  },
+  {
+    "revision": "1224436ef49e931584a2",
+    "url": "/videositemockup/static/js/main.227425b8.chunk.js"
+  },
+  {
+    "revision": "7ac7c576718cb9307f43",
+    "url": "/videositemockup/static/js/runtime-main.f3ce4dab.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/videositemockup/static/media/logo.25bf045c.svg"
+  }
+]);
